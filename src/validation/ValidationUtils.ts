27b10529f326dd05428f4f077e5582745f9fb13b
dung.nt <dung@hodfords.com>
2021-06-16 17:33:52 +0700

    Custom key

--- src/validation/ValidationUtils.ts
+++ src/validation/ValidationUtils.ts
@@ -16,32 +16,18 @@ export class ValidationUtils {
     message: string | ((args: ValidationArguments) => string),
     validationArguments: ValidationArguments
   ): string {
-    let messageString: string;
-    if (message instanceof Function) {
-      messageString = (message as (args: ValidationArguments) => string)(validationArguments);
-    } else if (typeof message === 'string') {
-      messageString = message;
-    }
 
-    if (messageString && validationArguments.constraints instanceof Array) {
+    let result: any = {
+      property: validationArguments.property,
+      target: validationArguments.targetName,
+      value: validationArguments.value
+    }
+    if (validationArguments.constraints instanceof Array) {
       validationArguments.constraints.forEach((constraint, index) => {
-        messageString = messageString.replace(
-          new RegExp(`\\$constraint${index + 1}`, 'g'),
-          constraintToString(constraint)
-        );
+        result[`constraint${index + 1}`] = constraintToString(constraint);
       });
     }
 
-    if (
-      messageString &&
-      validationArguments.value !== undefined &&
-      validationArguments.value !== null &&
-      typeof validationArguments.value === 'string'
-    )
-      messageString = messageString.replace(/\$value/g, validationArguments.value);
-    if (messageString) messageString = messageString.replace(/\$property/g, validationArguments.property);
-    if (messageString) messageString = messageString.replace(/\$target/g, validationArguments.targetName);
-
-    return messageString;
+    return result;
   }
 }
