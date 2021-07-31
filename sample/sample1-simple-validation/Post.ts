308e9a9c480c3a48d8225f7b584d4c9a11dec1d4
dung.nt <dung@hodfords.com>
2021-07-31 11:08:40 +0700

    Custom key

--- sample/sample1-simple-validation/Post.ts
+++ sample/sample1-simple-validation/Post.ts
@@ -1,49 +1,16 @@
-import {
-  Contains,
-  IsInt,
-  MinLength,
-  MaxLength,
-  IsEmail,
-  IsFQDN,
-  IsDate,
-  ArrayNotEmpty,
-  ArrayMinSize,
-  ArrayMaxSize,
-  IsEnum,
-} from '../../src/decorator/decorators';
+import { IsNotEmpty, IsObject, MaxLength, MinLength, ValidateNested } from '../../src/decorator/decorators';
 
-export enum PostType {
-  Public,
-  Private,
+class Test {
+  @MinLength(10)
+  title: string;
 }
 
 export class Post {
   @MinLength(10)
-  @MaxLength(20)
   title: string;
 
-  @Contains('hello')
-  text: string;
-
-  @IsInt()
-  rating: number;
-
-  @IsEmail()
-  email: string;
-
-  @IsFQDN()
-  site: string;
-
-  @IsDate()
-  createDate: Date;
-
-  @ArrayNotEmpty()
-  @ArrayMinSize(2)
-  @ArrayMaxSize(5)
-  @MinLength(3, { each: true, message: 'Tag is too short. Minimal length is $value characters' })
-  @MaxLength(50, { each: true, message: 'Tag is too long. Maximal length is $value characters' })
-  tags: string[];
-
-  @IsEnum(PostType)
-  type: PostType;
+  @IsNotEmpty()
+  @IsObject({ each: true })
+  @ValidateNested({ each: true })
+  titles: Test[];
 }
